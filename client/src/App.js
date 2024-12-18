3d8591c7851f3b1437221db61df019a77126a31d
cs-keni <kennn@uoregon.edu>
2024-12-17 21:13:38 -0800

    set up react, tailwind, and basic file structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center py-10">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+      <p className="mt-4">Tailwind CSS is successfully configured</p>
+    </div>
+  );
+}
+
+export default App;
