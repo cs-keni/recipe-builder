1db34ce2324d57dc81313f21d19b2e8dcdb4fc81
cs-keni <kennn@uoregon.edu>
2024-12-18 20:42:56 -0800

    just making sure app.py works

--- server/app.py
+++ server/app.py
@@ -4,7 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/recipes', methods=['GET'])
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to the Recipe Builder API!"})
+
+@app.route("/recipes")
 def get_recipes():
     return jsonify({"recipes": []})
 
