3ed2afaffc9d24d472ae10cea9e2c70da0bc9cdb
cs-keni <kennn@uoregon.edu>
2024-12-18 20:48:58 -0800

    changed up minimal react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white text-center py-10">
-      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
-      <p className="mt-4">Tailwind CSS is successfully configured</p>
-    </div>
+      <div className="App">
+	<header className="bg-blue-500 text-white text-center py-10">
+	  <h1 className="text-4xl font-bold">Recipe Builder</h1>
+	  <p className="mt-4">Start building your recipes from leftovers!</p>
+	</header>
+      </div>
   );
 }
 
